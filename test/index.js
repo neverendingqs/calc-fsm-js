ddff5d2e431fe31e1f3d1b2ba2570c093ecbe0ef
Mark Tse <mark.tse@neverendingqs.com>
2018-12-31 16:50:18 -0500

    lib - now does simple calculations.

--- test/index.js
+++ test/index.js
@@ -6,10 +6,10 @@ const digitsKeys = Object.values(digits);
 
 describe('calc-fsm', function() {
   beforeEach(function() {
-    this.cfsm = new Cfsm
+    this.cfsm = new Cfsm;
   });
 
-  describe('toDigit()', function() {
+  describe('managing state', function() {
     it('can transition from number to number', function() {
       digitsKeys.forEach( i => this.cfsm.toDigit(i) );
       assert.sameOrderedMembers(
@@ -44,6 +44,54 @@ describe('calc-fsm', function() {
         this.cfsm.history,
         [START, digits.nine, END]
       );
+    });
+  });
+
+  describe('it can calculate expressions', function() {
+    it('can add', function() {
+      this.cfsm.toDigit(digits.one);
+      this.cfsm.toOp(ops.ADD);
+      this.cfsm.toDigit(digits.one);
+      this.cfsm.end();
+      
+      const sum = this.cfsm.calculate();
+      assert.equal(sum, 2);
+    });
+
+
+    it('can subtract', function() {
+      this.cfsm.toDigit(digits.one);
+      this.cfsm.toDigit(digits.five)
+      this.cfsm.toOp(ops.SUBTRACT);
+      this.cfsm.toDigit(digits.three);
+      this.cfsm.end();
+      
+      const difference = this.cfsm.calculate();
+      assert.equal(difference, 12);
+    });
+
+
+    it('can multply', function() {
+      this.cfsm.toDigit(digits.three);
+      this.cfsm.toOp(ops.MULTIPLY);
+      this.cfsm.toDigit(digits.nine);
+      this.cfsm.end();
+      
+      const product = this.cfsm.calculate();
+      assert.equal(product, 27);
+    });
+
+
+    it('can divide', function() {
+      this.cfsm.toDigit(digits.three);
+      this.cfsm.toDigit(digits.seven);
+      this.cfsm.toDigit(digits.eight);
+      this.cfsm.toOp(ops.DIVIDE);
+      this.cfsm.toDigit(digits.two);
+      this.cfsm.end();
+      
+      const quotient = this.cfsm.calculate();
+      assert.equal(quotient, 189);
     });
   });
 });
